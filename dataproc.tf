8bedfaf048c1640505278f34eec024472f8ced47
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2023-11-01 14:36:16 +0530

    Update terraform config

--- dataproc.tf
+++ dataproc.tf
@@ -1,57 +1,68 @@
-resource "google_dataproc_cluster" "dataproc-cluster" {
-  name     = "dataproc-cluster"
-  region   = "us-central1"
-  graceful_decommission_timeout = "120s"
-  labels = {
-    foo = "bar"
-  }
+resource "google_service_account" "dataproc-svc" {
+  project      = var.project_id
+  account_id   = "dataproc-svc"
+  display_name = "Service Account - dataproc"
+}
+
+resource "google_project_iam_member" "svc-access" {
+  project = var.project_id
+  role    = "roles/dataproc.worker"
+  member  = "serviceAccount:${google_service_account.dataproc-svc.email}"
+}
+
+resource "google_storage_bucket" "dataproc-bucket" {
+  project                     = var.project_id
+  name                        = "${var.prefix}-dataproc-config"
+  uniform_bucket_level_access = true
+  location                    = var.region
+}
+
+resource "google_storage_bucket_iam_member" "dataproc-member" {
+  bucket = google_storage_bucket.dataproc-bucket.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.dataproc-svc.email}"
+}
+
+
+resource "google_dataproc_cluster" "mycluster" {
+  name                          = "${var.prefix}-dataproc"
+  region                        = var.region
 
   cluster_config {
-    staging_bucket = "dataproc-staging-bucket"
+    staging_bucket = google_storage_bucket.dataproc-bucket.name
 
     master_config {
       num_instances = 1
-      machine_type  = "e2-medium"
+      machine_type  = var.dataproc_master_machine_type
       disk_config {
-        boot_disk_type    = "pd-ssd"
-        boot_disk_size_gb = 30
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = var.dataproc_master_bootdisk
       }
     }
 
     worker_config {
-      num_instances    = 2
-      machine_type     = "e2-medium"
-      min_cpu_platform = "Intel Skylake"
+      num_instances = var.dataproc_workers_count
+      machine_type  = var.dataproc_worker_machine_type
       disk_config {
-        boot_disk_size_gb = 30
-        num_local_ssds    = 1
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = var.dataproc_worker_bootdisk
+        num_local_ssds    = var.worker_local_ssd
       }
     }
 
     preemptible_worker_config {
-      num_instances = 0
+      num_instances = var.preemptible_worker
     }
 
-    # Override or set some custom properties
     software_config {
-      image_version = "2.0.35-debian10"
-      override_properties = {
-        "dataproc:dataproc.allow.zero.workers" = "true"
-      }
+      image_version = "2.0.66-debian10"
     }
 
     gce_cluster_config {
-      tags = ["foo", "bar"]
-      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-      service_account_scopes = [
-        "cloud-platform"
-      ]
-    }
-
-    # You can define multiple initialization_action blocks
-    initialization_action {
-      script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
-      timeout_sec = 500
+      zone = "${var.region}-b"
+      subnetwork             = var.subnet_name
+      service_account        = google_service_account.dataproc-svc.email
+      service_account_scopes = ["cloud-platform"]
     }
   }
 }
